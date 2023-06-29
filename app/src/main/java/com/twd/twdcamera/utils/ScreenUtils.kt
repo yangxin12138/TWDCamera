cd722c78da896077df762107b32cc189f58ce0b3
yangxin12138 <995249880@qq.com>
2023-06-29 16:48:09 +0800

    尝试使用Kotlin

--- app/src/main/java/com/twd/twdcamera/utils/ScreenUtils.kt
+++ app/src/main/java/com/twd/twdcamera/utils/ScreenUtils.kt
@@ -1,56 +1,50 @@
-package com.twd.twdcamera.utils;
+package com.twd.twdcamera.utils
 
-import android.util.DisplayMetrics;
-import android.util.Log;
-import android.view.ViewGroup;
+import android.util.Log
+import android.view.ViewGroup
+import androidx.camera.view.PreviewView
 
-import androidx.camera.view.PreviewView;
-
-public class ScreenUtils {
-    PreviewView previewView;
-    int screenWidth;
-    int screenHeight;
-
-    public ScreenUtils(PreviewView previewView,int screenWidth,int screenHeight) {
-        this.previewView = previewView;
-        this.screenWidth = screenWidth;
-        this.screenHeight = screenHeight;
-    }
-
-
-
-    public  void updateSize(String item){
-        ViewGroup.LayoutParams layoutParams = previewView.getLayoutParams();
-
-        if ("1280*720".equals(item)) {
-            float aspectRatio4x3 = 4f / 3f;
-            int targetWidth4x3 = screenWidth;
-            int targetHeight4x3 = (int) (targetWidth4x3 / aspectRatio4x3);
+class ScreenUtils(var previewView: PreviewView, var screenWidth: Int, var screenHeight: Int) {
+    fun updateSize(item: String) {
+        val layoutParams = previewView.layoutParams
+        if ("1280*720" == item) {
+            val aspectRatio4x3 = 4f / 3f
+            var targetWidth4x3 = screenWidth
+            var targetHeight4x3 = (targetWidth4x3 / aspectRatio4x3).toInt()
             if (targetHeight4x3 > screenHeight) {
-                targetHeight4x3 = screenHeight;
-                targetWidth4x3 = (int) (targetHeight4x3 * aspectRatio4x3);
+                targetHeight4x3 = screenHeight
+                targetWidth4x3 = (targetHeight4x3 * aspectRatio4x3).toInt()
             }
-            layoutParams.width = targetWidth4x3;
-            layoutParams.height = targetHeight4x3;
-            previewView.setLayoutParams(layoutParams);
-            Log.i("yang", "------首页切换到720------" + "width:" + layoutParams.width + ",height:" + layoutParams.height);
-        } else if ("1920*1080".equals(item)) {
-            float aspectRatio16x9 = 16f / 9f;
-            int targetWidth16x9 = screenWidth;
-            int targetHeight16x9 = (int) (targetWidth16x9 / aspectRatio16x9);
+            layoutParams.width = targetWidth4x3
+            layoutParams.height = targetHeight4x3
+            previewView.layoutParams = layoutParams
+            Log.i(
+                "yang",
+                "------首页切换到720------" + "width:" + layoutParams.width + ",height:" + layoutParams.height
+            )
+        } else if ("1920*1080" == item) {
+            val aspectRatio16x9 = 16f / 9f
+            var targetWidth16x9 = screenWidth
+            var targetHeight16x9 = (targetWidth16x9 / aspectRatio16x9).toInt()
             if (targetHeight16x9 > screenHeight) {
-                targetHeight16x9 = screenHeight;
-                targetWidth16x9 = (int) (targetHeight16x9 * aspectRatio16x9);
+                targetHeight16x9 = screenHeight
+                targetWidth16x9 = (targetHeight16x9 * aspectRatio16x9).toInt()
             }
-            layoutParams.width = targetWidth16x9;
-            layoutParams.height = targetHeight16x9;
-            previewView.setLayoutParams(layoutParams);
-            Log.i("yang", "------首页切换到1080------" + "width:" + layoutParams.width + ",height:" + layoutParams.height);
-        } else if ("自动".equals(item)) {
-            layoutParams.width = ViewGroup.LayoutParams.MATCH_PARENT;
-            layoutParams.height = ViewGroup.LayoutParams.MATCH_PARENT;
-            previewView.setLayoutParams(layoutParams);
-            Log.i("yang", "------首页切换到Auto------" + "width:" + layoutParams.width + ",height:" + layoutParams.height);
+            layoutParams.width = targetWidth16x9
+            layoutParams.height = targetHeight16x9
+            previewView.layoutParams = layoutParams
+            Log.i(
+                "yang",
+                "------首页切换到1080------" + "width:" + layoutParams.width + ",height:" + layoutParams.height
+            )
+        } else if ("自动" == item) {
+            layoutParams.width = ViewGroup.LayoutParams.MATCH_PARENT
+            layoutParams.height = ViewGroup.LayoutParams.MATCH_PARENT
+            previewView.layoutParams = layoutParams
+            Log.i(
+                "yang",
+                "------首页切换到Auto------" + "width:" + layoutParams.width + ",height:" + layoutParams.height
+            )
         }
     }
 }
